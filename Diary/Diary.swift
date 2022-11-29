d3a8b2b32488882afb6682d0502e3dc8e678f774
sue <suekim999@naver.com>
2022-11-29 22:10:07 +0900

    [Add] 기본적인 레이아웃 시작

---
+++ Diary/Diary.swift
@@ -0,0 +1,16 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Gunter on 2021/09/11.
+//
+
+import Foundation
+
+struct Diary {
+  var uuidString: String
+  var title: String
+  var contents: String
+  var date: Date
+  var isStar: Bool
+}
